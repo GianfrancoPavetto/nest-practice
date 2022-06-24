e1dd904c4df786e20350b362231133b33203f9a7
GianfrancoPavetto <gianfranco@caramelpoint.com>
2022-06-24 10:52:44 -0300

    initial commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'listing messages';
+  }
+
+  @Post()
+  createMessage(@Body() body: any) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
+}
